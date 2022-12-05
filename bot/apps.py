0ccdca5d3ca36391b30480fa65110fd3a3c48621
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-12-05 08:25:39 +0100

    auto run for telethon

--- bot/apps.py
+++ bot/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class BotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bot'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bot"
+
+    def ready(self):
+        import bot.telethon_bot
