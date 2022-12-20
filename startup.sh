ae41d119a8ed1513e62a1ccb15a57185f4f7f970
ifesinachi ugwuoke <ifexalex2@gmail.com>
2022-12-20 08:50:23 +0100

    update startupsh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 export LANG=C.UTF-8
 
-gunicorn --bind=0.0.0.0 --timeout 600 telebot.wsgi & python manage.py telegrambot1
+gunicorn --bind=0.0.0.0 --timeout 600 telebot.wsgi & python manage.py telegrambot1 & python manage.py telegrambot
