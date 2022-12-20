203777bc1d443309daa612db32bf9791e8f3f2c7
Ifesinachi Ugwuoke <ifexalex2@gmail.com>
2022-12-20 13:37:04 +0100

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 telegrambot.wsgi & pyhton manage.py telegrambot & pyhton manage.py telegrambot1 --access-logfile '-' --error-logfile '-'
+gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 telegrambot.wsgi & python manage.py telegrambot & python manage.py telegrambot1 --access-logfile '-' --error-logfile '-'
